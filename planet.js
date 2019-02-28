a6288888cc8048f0085f40913c114a0893409db6
Nacho <narchercher@gmail.com>
2019-02-28 13:33:37 +0100

    Sistema solar

---
+++ planet.js
@@ -0,0 +1,23 @@
+function Planet(radius, position, velocity) {
+
+  this.a = 0;
+  this.x = position;
+  this.y = 0;
+
+  this.update = function() {
+    this.x = position*cos(this.a);
+    this.y = position*sin(this.a);
+    this.a += velocity;
+  }
+
+  this.show = function() {
+    noStroke();
+    ellipse(this.x,this.y,radius,radius);
+  }
+  this.showRadius = function() {
+    strokeWeight(1);
+    noFill()
+    stroke(255);
+    ellipse(0,0,position*2,position*2);
+  }
+}
